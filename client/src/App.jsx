5fd3dd9a63e5a624c33797e511ce1da253c59637
AdemMlayah <adammlayah140@gmail.com>
2023-12-12 18:42:27 +0100

    ArtistPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,21 @@ import {
   Route,
 } from "react-router-dom";
 import { RouterProvider } from "react-router";
-
+import ArtistPage from './pages/artistProfile';
 function App() {
     const current = useContext(MyContext)
     const router = createBrowserRouter(
       createRoutesFromElements(
         <>
           <Route path='/' element={<h1>this is home</h1>} />
-           
+          <Route path='/artist' element={<ArtistPage/>}/>           
         </>
       )
     );
   return (
-   <> <div className="App ">
+
+   <>
+    <div className="App ">
    
      <RouterProvider router={router}></RouterProvider>
    
